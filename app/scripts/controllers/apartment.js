7aa65a4881a5eeb86ad92d4894df3607cbe4091c
Brian Blackwell <industriousthought@gmail.com>
2015-04-29 15:44:38 -0400

    Went ahead and commited the entire directory structure

--- app/scripts/controllers/apartment.js
+++ app/scripts/controllers/apartment.js
@@ -10,5 +10,6 @@
 angular.module('zyringApp')
 .controller('ApartmentCtrl', ['$scope', '$routeParams', 'Apartments', function ($scope, $routeParams, apartments) {
     $scope.cityName = $routeParams.cityName;
-    $scope.listings = apartments.listing.get({'city': $scope.cityName});
+    $scope.listings = apartments.listing.get({city: $scope.cityName});
+    console.log($scope.listings);
 }]);
